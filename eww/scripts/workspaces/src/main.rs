2a3f8e34e7d76fb660c39259526ef0ba74522af1
Louis Delyon <louis.delyon+git@proton.me>
2024-09-27 10:05:44 +0200

    Sorted workspaces in script

--- eww/scripts/workspaces/src/main.rs
+++ eww/scripts/workspaces/src/main.rs
@@ -1,4 +1,5 @@
 use hyprland::{
+    data::Workspace,
     event_listener::EventListener,
     shared::{HyprData, HyprDataActive},
 };
@@ -13,10 +14,15 @@ fn print_button(workspace_id: usize, is_active: bool) {
 }
 
 fn handler(_: impl std::any::Any) {
-    let workspaces = hyprland::data::Workspaces::get().unwrap();
+    let workspaces = hyprland::data::Workspaces::get()
+        .unwrap();
+    let mut workspaces_vec = workspaces.iter()
+        .filter(|w| 0 < w.id && w.id < 100)
+        .collect::<Vec<&Workspace>>();
+    workspaces_vec.sort_by_key(|w| w.id);
     let active_workspace = hyprland::data::Workspace::get_active().unwrap();
     print!("(box  :class \"workspaces\" :space-evenly false");
-    for workspace in workspaces.iter().filter(|w| 0 < w.id && w.id < 100) {
+    for workspace in workspaces_vec {
         print_button(workspace.id as usize, workspace.id == active_workspace.id);
     }
     println!(")");
