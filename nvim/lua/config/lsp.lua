aaa03eb74f28627f291e3901f581206930056f68
Louis Delyon <louis.delyon+git@proton.me>
2025-03-06 23:21:55 +0100

    Winter 2025

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,1 +1,13 @@
-return { "clangd", "rust_analyzer", "pylsp", "nil_ls", "cssls", "dockerls", "docker_compose_language_service", "jdtls", "lua_ls" }
+return {
+	"clangd",
+	"rust_analyzer",
+	"jedi_language_server",
+	"nil_ls",
+	"cssls",
+	"dockerls",
+	"docker_compose_language_service",
+	"jdtls",
+	"lua_ls",
+	"tinymist",
+	"ts_ls",
+}
